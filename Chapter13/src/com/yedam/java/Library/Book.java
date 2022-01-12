d23dd708791c68ed8be304a81a8ce7d123429b77
admin <admin@YD5-15>
2022-01-12 17:45:45 +0900

    2022.01.12 Th

---
+++ Chapter13/src/com/yedam/java/Library/Book.java
@@ -0,0 +1,50 @@
+package com.yedam.java.Library;
+
+public class Book {
+	String bookName;
+	String writer;
+	int serialNum;
+	boolean borrow;
+	
+	
+	
+	
+	
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public int getSerialNum() {
+		return serialNum;
+	}
+	public void setSerialNum(int serialNum) {
+		this.serialNum = serialNum;
+	}
+	public boolean isBorrow() {
+		return borrow;
+	}
+	public void setBorrow(boolean borrow) {
+		this.borrow = borrow;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
